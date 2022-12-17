56e2327d58cf785b3d23e9ccf963cde3a24532b6
rstar <rstar0823@icloud.com>
2022-12-17 16:19:49 +0900

    feat: 文字列検索スクリプトを実装

--- findgrep/findgrep.sh
+++ findgrep/findgrep.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+pattern=$1
+directory=$2
+name=$3
+
+# 第二引数(起点ディレクトリ)が空文字の場合、
+# デフォルト値としてカレントディレクトリを設定
+if [ -z "$directory" ]; then
+    directory='.'
+fi
+
+# 第三匹数が(検索ファイルパターン)が空文字の場合、
+# デフォルト値として全てのファイルを設定
+if [ -z "$name" ]; then
+    name='*'
+fi
+
+find "$directory" -type f -name "$name" | xargs grep -nH "$pattern"
