d5de2dd0fffeaf88ea2cc8c1aa0321578a4ae0e3
rstar <rstar0823@icloud.com>
2022-12-17 15:52:59 +0900

    add: tree.sh

--- tree/tree.sh
+++ tree/tree.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+list_recursive()
+{
+	local filepath=$1
+	local indent=$2
+
+	# インデント付きで、パス部分を取り除いてファイル名を表示する
+	echo "${indent}${filepath##*/}"
+
+	if [ -d "$filepath" ]; then
+		local filename
+
+		_IFS=$IFS
+		IFS=$'\n'
+		for filename in $(ls "$filepath")
+		do
+			list_recursive "${filepath}/${filename}" "  $indent"
+		done
+		IFS=$_IFS
+	fi
+}
+
+list_recursive "$1" ""
